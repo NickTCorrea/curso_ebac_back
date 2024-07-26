14012ee5300169bb5a089b6ef5b30596d12493c7
NickTCorrea <tcorrea.nick@gmail.com>
2024-07-26 08:32:18 -0300

    Realizando alterações no projeto inserando o scanner conforme solicitado pelo tutor.

--- Tarefa_Wrapper/src/br/com/nicolas/TarefaWrapper.java
+++ Tarefa_Wrapper/src/br/com/nicolas/TarefaWrapper.java
@@ -1,11 +1,20 @@
 package br.com.nicolas;
 
+import java.util.Scanner;
+
 public class TarefaWrapper {
     public static void main(String[] args) {
-        int valorNumero = 100;
-        System.out.println(valorNumero);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe um valor número, por gentileza: ");
+        int valorNumero = scanner.nextInt();
+        System.out.println("Valor numérico: " + valorNumero);
+
+        System.out.println("Convertendo...");
         Integer valorWrapper = valorNumero;
-        System.out.println(valorWrapper);
+        System.out.println("Valor convertido em Wrapper: " + valorWrapper);
+
+        scanner.close();
 
     }
 }
