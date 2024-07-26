313935aabc48aea57e8a3a6df138e671c28cf251
NickTCorrea <tcorrea.nick@gmail.com>
2024-07-26 06:01:27 -0300

    Adicionando a tarefa do módulo 9.

---
+++ Tarefa_Wrapper/src/br/com/nicolas/TarefaWrapper.java
@@ -0,0 +1,11 @@
+package br.com.nicolas;
+
+public class TarefaWrapper {
+    public static void main(String[] args) {
+        int valorNumero = 100;
+        System.out.println(valorNumero);
+        Integer valorWrapper = valorNumero;
+        System.out.println(valorWrapper);
+
+    }
+}
