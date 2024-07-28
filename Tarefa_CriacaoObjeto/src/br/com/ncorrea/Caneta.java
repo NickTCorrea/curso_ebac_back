26b87e43bff63d8d67e3363791cd8a4a8ab330fb
NickTCorrea <tcorrea.nick@gmail.com>
2024-07-28 00:55:49 -0300

    Adicionando a tarefa do módulo 7.

---
+++ Tarefa_CriacaoObjeto/src/br/com/ncorrea/Caneta.java
@@ -0,0 +1,25 @@
+package br.com.ncorrea;
+
+/**
+ * @author nicolas.correa
+ */
+public class Caneta {
+    //Atributos
+    String modelo;
+    String cor;
+    double ponta;
+    double tamanho;
+
+    //Metodos
+    public void escrever() {
+        //código para a caneta escrever
+    }
+
+    public void tampar() {
+        //código para tampar a caneta
+    }
+
+    public void destampar() {
+        //código para destampar a caneta
+    }
+}
